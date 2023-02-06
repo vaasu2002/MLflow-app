fe7d7a9d96ac9cc8bcc118f2277963907cae9bc1
vaasu2002 <vaasu.bisht2021@vitbhopal.ac.in>
2023-02-06 20:38:35 +0530

    base model update and notebook

--- src/base_model.py
+++ src/base_model.py
@@ -8,7 +8,9 @@ from src.utils.model import log_model_summary
 import random
 import tensorflow as tf
 
-STAGE = "BASE MODEL CREATION" ## <<< change stage name 
+STAGE = "BASE_MODEL_CREATION"
+
+os.makedirs("logs",exist_ok = True)
 
 logging.basicConfig(
     filename=os.path.join("logs", 'running_logs.log'), 
@@ -19,12 +21,36 @@ logging.basicConfig(
 
 
 def main(config_path):
-    ## read config files
+    IMG_SIZE = [180,180]
+    BATCH_SIZE = 32
+    # read config files
     config = read_yaml(config_path)
 
     params = config["params"]
 
     logging.info("layers defined")
+
+    DATA_DIR = "data/unzip/PetImages"
+
+
+    train_ds = tf.keras.preprocessing.image_dataset_from_directory(
+    DATA_DIR,
+    validation_split=0.2,
+    subset="training",
+    seed=42,
+    image_size=IMG_SIZE,
+    batch_size=BATCH_SIZE
+    )
+
+    val_ds = tf.keras.preprocessing.image_dataset_from_directory(
+        DATA_DIR,
+        validation_split=0.2,
+        subset="validation",
+        seed=42,
+        image_size=IMG_SIZE,
+        batch_size=BATCH_SIZE
+    )
+
     LAYERS = [
     tf.keras.layers.Input(shape=tuple(params["img_shape"])),
     tf.keras.layers.Conv2D(filters=32, kernel_size=(3,3), activation="relu"),
@@ -37,7 +63,11 @@ def main(config_path):
     ]
 
     classifier = tf.keras.Sequential(LAYERS)
-    print(classifier.summary())
+
+    logging.info(classifier.summary())
+
+    train_ds = train_ds.prefetch(buffer_size=32)
+    val_ds = val_ds.prefetch(buffer_size=32)
 
     # logging.info(f"base model summary:\n{log_model_summary(classifier)}")
 
@@ -47,6 +77,11 @@ def main(config_path):
     metrics=params["metrics"]
     )
 
+    EPOCHS = 1
+    history  = classifier.fit(train_ds, epochs=EPOCHS, validation_data = val_ds)
+
+    logging.info(history.history)
+    
     path_to_model_dir = os.path.join(
         config["data"]["local_dir"],
         config["data"]["model_dir"])
